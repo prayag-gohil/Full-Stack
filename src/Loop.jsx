8eb7c12ff0de40d5b0ad09aced5bfdec22a21b5f
prayag gohil <prayaggohil77@gmail.com>
2026-07-15 15:51:10 +0530

    add featucher search to show data in tabel

--- src/Loop.jsx
+++ src/Loop.jsx
@@ -1,40 +1,70 @@
 // looping using map()
 // array of object , array
+import {useState , useMemo} from "react";
 export default function Loop()
 {
  //array
 const user=["abc","xyz","jkl","pqr","lmn"];
-const user_data=[
-    {'id':101,"name":"abc","age":20},
-    {'id':102,"name":"xyz","age":21},
-    {'id':103,"name":"pqr","age":22},
+
+const [search, setSearch] = useState("");
+//filtering data using filter method 
+//filtering array of object based on condition
+//const filtered = user_data.filter((element) => element.role.toLowerCase() === search.toLowerCase());
+const filtered = useMemo(() =>{
+    const user_data=[
+    {'id':101,"name":"abc","age":20,"role":"admin"},
+    {'id':102,"name":"xyz","age":21,"role":"developer"},
+    {'id':103,"name":"pqr","age":22,"role":"ceo"},
 ];
+    return user_data.filter((element) => element.role.toLowerCase() === search.toLowerCase());
+   // other way return user_data.filter((element) => element.role.toLowerCase().includes(search.toLowerCase()));
+},[search]);
     return(
         <>
-        <ol>
+
+        
+    
+         <ol>
             {user.map((element,index)=>(
                 <li key={index}>{element}</li>
             ))}
         </ol>
 
+        <input type="text" name="search" placeholder="search role" value={search} onChange={(e)=>setSearch(e.target.value)}/> 
+        
         <table border={1}>
             <thead>
                 <tr>
                     <td>ID</td>
                     <td>Name</td>
                     <td>Age</td>
+                    <td>Role</td>
                 </tr>
             </thead>
             <tbody>
-                {user_data.map((element)=>(
-                    <tr key={element.id}>
-                        <td>{element.id}</td>
-                        <td>{element.name}</td>
-                        <td>{element.age}</td>
+                {filtered.length > 0 ? (
+                    filtered.map((element)=>(
+                        <tr key={element.id}>
+                            <td>{element.id}</td>
+                            <td>{element.name}</td>
+                            <td>{element.age}</td>
+                            <td>{element.role}</td>
+                        </tr>
+                    ))
+                ) : (
+                    <tr>
+                        <td>No results found</td>
                     </tr>
-                ))}
+                )}
             </tbody>
         </table>
         </>
     )
 }
+
+
+
+
+
+
+
