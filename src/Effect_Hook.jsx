4296c49469fcf0d47f13d02b365964f5aba93555
prayag gohil <prayaggohil77@gmail.com>
2026-07-15 16:27:36 +0530

    chnage in effect hook file

--- src/Effect_Hook.jsx
+++ src/Effect_Hook.jsx
@@ -1,22 +1,32 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 export default function Effect_Hook()
 {
     //state variable
     const [count, setCount] = useState(0);
+    const [stock, setStock] = useState(20);
+
     function increment(){
         setCount(count+1);
+    }
+    function decrement(){
+        setStock(stock-1);
     }
     function display(){
         console.log("useEffect Example");
     }
-    display();
+    useEffect(()=>{
+        display();
+    },[count,stock]);
+    //display();
     return(
         <>
             <h1>useEffect Example</h1>
             <p>Count: {count}</p>
             <button onClick={increment}>Increment</button>
 
+            <button onClick={decrement}>stock</button>
+            <p>Stock: {stock}</p>
         </>
     )
 }
